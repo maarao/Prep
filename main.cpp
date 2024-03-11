d5dd0f090bc1bb4b027052f09dafbf5fc0f63644
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-11 17:17:45 -0400

    Made things run locally!

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "longest-substring-without-repeating-characters.hpp"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << Solution::lengthOfLongestSubstring("abcabcbb") << endl;
+    return 0;
+}
